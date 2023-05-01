2b16e31ca59674d2e16bd0ae7a7fdd2ef847bd92
Fatur Rahman <frfatram@gmail.com>
2023-05-02 00:06:17 +0700

    edit documentation

--- controller/http/position/handler.go
+++ controller/http/position/handler.go
@@ -8,8 +8,12 @@ import "github.com/labstack/echo/v4"
 // @Tags         	Position
 // @Accept       	json
 // @Produce      	json
+// @Param Authorization header string true "Insert your access token" default(Bearer <Add access token here>)
 // @Param			page number			query	int		false	"page"
 // @Param			page size			query	int		false	"limit"
+// @Param			description			query	int		false	"description"
+// @Param			location			query	int		false	"location"
+// @Param			full time			query	bool		false	"full_time"
 // @Success      	200  {object}   []model.Position
 // @Failure      	400,401,500  {object}  	pkg.Error
 // @Router       	/positions [get]
@@ -23,6 +27,7 @@ func (ctr PositionController) GetAll(c echo.Context) error {
 // @Tags         	Position
 // @Accept       	json
 // @Produce      	json
+// @Param Authorization header string true "Insert your access token" default(Bearer <Add access token here>)
 // @param 	   		id		path 	string 	true 	"id position"
 // @Success      	200  {object}   model.Position
 // @Failure      	400,401,500  {object}  	pkg.Error
